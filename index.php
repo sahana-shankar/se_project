eb9d5aefbec103852d54b0e02c1b2e720d1c2875
sahana-shankar <115129880+sahana-shankar@users.noreply.github.com>
2022-11-30 10:13:27 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 <?php endif;?>
 <div class="card card-outline rounded-0 card-navy">
 	<div class="card-header">
-		<h3 class="card-title">List of Vehicle Categories</h3>
+		<h3 class="card-title">List of Pass History</h3>
 		<div class="card-tools">
-			<a href="javascript:void(0)" id="create_new" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New</a>
+			<a href="./?page=pass_history/manage_pass_history" id="create_new" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New</a>
 		</div>
 	</div>
 	<div class="card-body">
@@ -17,45 +17,48 @@
 				<colgroup>
 					<col width="5%">
 					<col width="15%">
-					<col width="50%">
+					<col width="25%">
 					<col width="15%">
 					<col width="15%">
+					<col width="15%">
+					<col width="10%">
 				</colgroup>
 				<thead>
 					<tr>
 						<th>#</th>
 						<th>Date Created</th>
-						<th>Name</th>
-						<th>Status</th>
+						<th>Pass Code/Owner</th>
+						<th>Category</th>
+						<th>Toll</th>
+						<th>Cost</th>
 						<th>Action</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php 
 					$i = 1;
-						$qry = $conn->query("SELECT * from `category_list` where delete_flag = 0 order by abs(`name`) asc, `name` asc ");
-						while($row = $qry->fetch_assoc()):
+					$uwhere="";
+					if($_settings->userdata('type') != 1)
+					$uwhere =" WHERE p.user_id = '{$_settings->userdata('id')}' AND  p.delete_flag = 0";
+					$qry = $conn->query("SELECT ph.*, p.code, p.vehicle_name, p.owner, c.name as category, t.name as `toll` from `pass_history` ph inner join pass_list p on ph.pass_id = p.id inner join category_list c on p.category_id = c.id inner join `toll_list` t on ph.toll_id = t.id {$uwhere}  order by unix_timestamp(ph.`date_created`) desc ");
+					while($row = $qry->fetch_assoc()):
 					?>
 						<tr>
 							<td class="text-center"><?php echo $i++; ?></td>
 							<td><?php echo date("Y-m-d H:i",strtotime($row['date_created'])) ?></td>
-							<td><?php echo $row['name'] ?></td>
-							<td class="text-center">
-                                <?php if($row['status'] == 1): ?>
-                                    <span class="badge badge-success px-3 rounded-pill">Active</span>
-                                <?php else: ?>
-                                    <span class="badge badge-danger px-3 rounded-pill">Inactive</span>
-                                <?php endif; ?>
-                            </td>
+							<td><?php echo $row['code']." - " .$row['owner'] ?></td>
+							<td><?php echo $row['category'] ?></td>
+							<td><?php echo $row['toll'] ?></td>
+							<td class="text-right"><?php echo number_format($row['cost']) ?></td>
 							<td align="center">
 								 <button type="button" class="btn btn-flat p-1 btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
 				                  		Action
 				                    <span class="sr-only">Toggle Dropdown</span>
 				                  </button>
 				                  <div class="dropdown-menu" role="menu">
-				                    <a class="dropdown-item view_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-eye text-dark"></span> View</a>
+				                    <a class="dropdown-item view_data" href="./?page=pass_history/view_pass_history&id=<?php echo $row['id'] ?>"><span class="fa fa-eye text-dark"></span> View</a>
 				                    <div class="dropdown-divider"></div>
-				                    <a class="dropdown-item edit_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
+				                    <a class="dropdown-item edit_data" href="./?page=pass_history/manage_pass_history&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
 				                    <div class="dropdown-divider"></div>
 				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
 				                  </div>
@@ -70,16 +73,7 @@
 <script>
 	$(document).ready(function(){
 		$('.delete_data').click(function(){
-			_conf("Are you sure to delete this Category permanently?","delete_category",[$(this).attr('data-id')])
-		})
-		$('#create_new').click(function(){
-			uni_modal("<i class='fa fa-plus'></i> Add New Category","categories/manage_category.php")
-		})
-		$('.view_data').click(function(){
-			uni_modal("<i class='fa fa-bars'></i> Category Details","categories/view_category.php?id="+$(this).attr('data-id'))
-		})
-		$('.edit_data').click(function(){
-			uni_modal("<i class='fa fa-edit'></i> Update Category Details","categories/manage_category.php?id="+$(this).attr('data-id'))
+			_conf("Are you sure to delete this pass_history permanently?","delete_pass_history",[$(this).attr('data-id')])
 		})
 		$('.table').dataTable({
 			columnDefs: [
@@ -89,10 +83,10 @@
 		});
 		$('.dataTable td,.dataTable th').addClass('py-1 px-2 align-middle')
 	})
-	function delete_category($id){
+	function delete_pass_history($id){
 		start_loader();
 		$.ajax({
-			url:_base_url_+"classes/Master.php?f=delete_category",
+			url:_base_url_+"classes/Master.php?f=delete_pass_history",
 			method:"POST",
 			data:{id: $id},
 			dataType:"json",
